e83afdd2d97fc2f4d4aee4355880f977d9134318
julian fajardo <julian10fajardo@gmail.com>
2023-05-01 02:32:20 -0500

    Testing methods complete task and delete task

--- IAFapp/IAFBenflex/urls.py
+++ IAFapp/IAFBenflex/urls.py
@@ -4,12 +4,9 @@ from django.urls import path
 from django import views
 from . import views
 
-# Urls for users and pages
+# routes for users django
 urlpatterns = [
-    path('home/', views.landingPages, name='home'),
     path('signIn/', views.user_login, name='signIn'),
     path('create_user/', views.create_user, name='register'),
-    path('edit_user/', views.edit_user, name='edit'),
     path('logout/', views.signout, name='logout'),
-    path('delete_user/', views.delete_user, name='delete'),
 ]
